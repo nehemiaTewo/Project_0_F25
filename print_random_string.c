b6a8e0c4395fff7f65ddba0bc41cd6ab81f77cff
nehemiatew <nehemiabelete7@gmail.com>
2025-09-02 09:42:30 -0400

    Competed rand string

--- print_random_string.c
+++ print_random_string.c
@@ -7,31 +7,44 @@
 
 
 // random string generator wrapper function
-char* rand_string_alloc(size_t size)
-{
-     char *s = malloc(size + 1);
-     if (s) {
-         rand_string(s, size); // to be developed by the student
-     }
-     return s;
+char* rand_string_alloc(size_t size) {
+    char *s  = malloc(size + 1);
+    if(s){
+        rand_string(s, size);
+    }
+    return s;
 }
 
 
 
-int main()
 
-{
+void rand_string(char *s, size_t size){
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    for (size_t i = 0; i < size ; i++) {
+        int key = rand() % (int) (sizeof(charset) -1);
+        s[i] = charset[key];
+        }
+        s[size] = "\0";
+    } 
+
+     
+
+
+int main() {
     char *str;
-    size_t size =   ;  //specify a value for size of word
+    size_t size = 10;  //specify a value for size of word
 
 	/* seed the randomizer */
 	srand( (unsigned)time(NULL) );
 
 	printf("Today's random word: ");
     str = rand_string_alloc(size );
-    printf("%s",str);
-    free (str);
-    
+    if (str) {
+        printf("%s",str);
+        free (str);
+    } else {
+        printf("invalid.\n");
+    }
 
 	return(0);
 }
